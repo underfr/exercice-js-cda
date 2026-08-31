23d1c97794a06456306f23bf42df49a4f9c9c38c
underfr <loicklaurent28@gmail.com>
2026-08-31 16:50:11 +0200

    Add styling ex-dom

--- src/pages/exercices/ex-dom.js
+++ src/pages/exercices/ex-dom.js
@@ -7,7 +7,7 @@ let paragraphContent = "Lorem ipsum"
 h1Title.textContent = h1TitleContent
 paragraph.textContent = paragraphContent
 
-h1Title.style.backgroundColor = "blue"
+h1Title.classList.add('text-4xl', 'font-bold', 'p-8', 'text-primary')
 
 const userData = {
     name: 'John delavega',
@@ -22,21 +22,27 @@ let userDiv = document.querySelector('.userProfile')
 
 const userImg = document.createElement('img')
 userImg.src = userData.img
+userImg.classList.add('rounded-full', 'w-32', 'h-32', 'object-cover', 'mb-4')
 
 const userName = document.createElement('p')
 userName.textContent = userData.name
+userName.classList.add('text-xl', 'font-bold', 'mb-2')
 
 const userEmail = document.createElement('p')
 userEmail.textContent = userData.email
+userEmail.classList.add('text-sm', 'text-base-content/70', 'mb-2')
 
 const userAge = document.createElement('p')
 userAge.textContent = userData.age
+userAge.classList.add('text-sm', 'mb-2')
 
 const userDob = document.createElement('p')
 userDob.textContent = userData.dob
+userDob.classList.add('text-sm', 'mb-2')
 
 const userOnline = document.createElement('p')
 userOnline.textContent = userData.active ? "Online" : "Offline"
+userOnline.classList.add('badge', 'badge-success')
 
 userDiv.append(userImg)
 userDiv.append(userName)
