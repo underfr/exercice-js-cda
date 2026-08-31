dd33562521d57dd6c0f50e7a0a0ec23bd561d1ba
underfr <loicklaurent28@gmail.com>
2026-08-31 15:53:09 +0200

    Add initial project files

---
+++ src/pages/exercices/ex-dom.js
@@ -0,0 +1,10 @@
+let h1Title = document.querySelector('h1')
+let paragraph = document.querySelector('p')
+
+let h1TitleContent = "Exercices DOM"
+let paragraphContent = "Lorem ipsum"
+
+h1Title.textContent = h1TitleContent
+paragraph.textContent = paragraphContent
+
+h1Title.style.backgroundColor = "blue"
---
+++ src/pages/exercices/ex-variables.js
@@ -0,0 +1,25 @@
+console.log("---------Exercices variables---------")
+
+let myAge = 10
+console.log(myAge)
+
+let myTab = [1,2,3,4,5,6]
+console.log(myTab[3])
+
+let myFloat = 10.50
+console.log(myFloat)
+
+let myText = "lorem ipsum"
+console.log(myText)
+
+let myObject = {
+    name: "Loïck",
+    age: "27"
+}
+console.log(myObject.name, myObject.age)
+
+let helloWorld = ()=>{
+   return "Hello World"
+}
+
+console.log(helloWorld())
